7720fba15e880f22e560a113bc29e3eaa0928227
Pritiranjan Pradhan <ppritiranjan7@gmail.com>
2025-04-15 09:18:12 +0530

    setup

---
+++ src/app/(main)/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+export default function TabLayout() {
+
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="todo"
+        options={{
+          title: 'Todos',
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/(main)/(tabs)/todo.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <View>
+      <Text>T</Text>
+    </View>
+  )
+}
+
+export default Todo;
